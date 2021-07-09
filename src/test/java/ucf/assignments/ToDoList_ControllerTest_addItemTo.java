c4f42913cc04640ff18738b575b86ab54048893f
C.Gagne <crystal.r.gagne@gmail.com>
2021-07-09 11:05:56 -0400

    Created Test for adding items to the list

--- src/test/java/ucf/assignments/ToDoList_ControllerTest_addItemTo.java
+++ src/test/java/ucf/assignments/ToDoList_ControllerTest_addItemTo.java
@@ -5,17 +5,35 @@
 
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToDoList_ControllerTest_addItemTo {
 
     @Test
-    void addItemToList_AddNewItem_CheckNewListIsCorrect_AssertTrue()
+    void addItemToList_AddNewItem_CheckNewListSizeIsCorrect_AssertTrue()
     {
         // Create list
         // add item to list using this function
         // verify list size is equal to original list, plus new item
+
+        ObservableList<SingleToDo> listForTesting = FXCollections.observableArrayList();
+        listForTesting.add(new SingleToDo(false, LocalDate.now(), "Difo was never a god."));
+        listForTesting.add(new SingleToDo(true, LocalDate.now(), "Go to class"));
+
+        Edit_ObservableList listEditor = new Edit_ObservableList();
+        listForTesting = listEditor.addItemToList(listForTesting);
+
+        int expected = 3;
+        int actual = listForTesting.size();
+
+        assertEquals(expected,actual);
+
+
     }
 }
