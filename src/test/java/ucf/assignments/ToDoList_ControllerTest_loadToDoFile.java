58be27575ebd746e2731cf5628b194cd14b6a752
C.Gagne <crystal.r.gagne@gmail.com>
2021-07-09 10:32:43 -0400

    Test for LoadFile was created

--- src/test/java/ucf/assignments/ToDoList_ControllerTest_loadToDoFile.java
+++ src/test/java/ucf/assignments/ToDoList_ControllerTest_loadToDoFile.java
@@ -6,16 +6,28 @@
 
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToDoList_ControllerTest_loadToDoFile {
 
     @Test
-    void loadToDoFile_CheckFileExists_AssertTrue()
+    void loadToDoFile_CheckDescriptionIsLoaded_AssertEquals()
     {
         // verify the file can be opened
         // This could instead test the Class LoadFile
+        ObservableList<SingleToDo> listForTesting = FXCollections.observableArrayList();
+        File loadedFile = new File("src/test/java/ucf/assignments/test/loadTest.csv");
+
+        LoadFile loadFile = new LoadFile();
+        listForTesting = loadFile.writeSingleToDoExcel(loadedFile);
+        String expected = "Difo was never a god.";
+        String actual = listForTesting.get(0).getDescription();
+        assertEquals(expected,actual);
     }
 }
